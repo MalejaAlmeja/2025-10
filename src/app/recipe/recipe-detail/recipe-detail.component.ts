2b9eea724a658b4e96e730ee9c636872d2f7d308
lgomezo <123671148+lgomezo@users.noreply.github.com>
2025-05-16 07:53:19 -0500

    changes

--- src/app/recipe/recipe-detail/recipe-detail.component.ts
+++ src/app/recipe/recipe-detail/recipe-detail.component.ts
@@ -1,14 +1,45 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Recipe } from '../Recipe';
+import { RecipeService } from '../recipe.service';
+import { CommonModule } from '@angular/common';
+import { IngredientModule } from '../../ingredient/ingredient.module';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-recipe-detail',
-  standalone: false,
+  standalone: false, // Change this to false since we're using it in a module
   templateUrl: './recipe-detail.component.html',
-  styleUrl: './recipe-detail.component.css',
+  styleUrls: ['./recipe-detail.component.css'],
 })
 export class RecipeDetailComponent implements OnInit {
-  @Input() recipe: any;
+  recipe: Recipe | null = null;
 
-  constructor() {}
-  ngOnInit(): void {}
+  constructor(
+    private route: ActivatedRoute,
+    private recipeService: RecipeService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      const id = +params['id'];
+      this.getRecipeDetail(id);
+    });
+  }
+
+  getRecipeDetail(id: number): void {
+    this.recipeService.getRecipeDetail(id).subscribe({
+      next: (recipe) => {
+        this.recipe = recipe;
+      },
+      error: (error) => {
+        console.error('Error fetching recipe detail:', error);
+      },
+    });
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
--- src/app/recipe/recipe.module.ts
+++ src/app/recipe/recipe.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { IngredientModule } from '../ingredient/ingredient.module';
+import { HttpClientModule } from '@angular/common/http';
+import { RecipeService } from './recipe.service';
 
 @NgModule({
-  declarations: [RecipeListComponent, RecipeDetailComponent],
-  imports: [CommonModule, IngredientModule],
-  exports: [RecipeListComponent, RecipeDetailComponent],
+  declarations: [
+    RecipeListComponent,
+    RecipeDetailComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    HttpClientModule,
+    IngredientModule
+  ],
+  exports: [
+    RecipeListComponent,
+    RecipeDetailComponent
+  ],
+  providers: [RecipeService]
 })
-export class RecipeModule {}
+export class RecipeModule { }
